7d7a921419dbc3952c31248b340a5114361da9c8
Darwin Do <darwin78913@gmail.com>
2025-02-10 01:39:35 -0500

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/config/lazy.lua Add .config/nvim/lua/plugins/disabled.lua Add .config/nvim/lua/plugins/mini.lua Add .config/nvim/lua/plugins/snacks-explorer.lua Add .config/nvim/lua/plugins/snacks-picker.lua

--- private_dot_config/nvim/lua/config/lazy.lua
+++ private_dot_config/nvim/lua/config/lazy.lua
@@ -31,6 +31,8 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.coding.mini-surround" },
     { import = "lazyvim.plugins.extras.test.core" },
+    { import = "lazyvim.plugins.extras.editor.snacks_explorer" },
+    { import = "lazyvim.plugins.extras.editor.snacks_picker" },
     { import = "plugins" },
   },
   defaults = {
