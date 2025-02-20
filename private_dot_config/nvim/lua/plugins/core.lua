7fd129a7d19a9c1164db5cee2cea5c0642e506e7
Darwin Do <darwin78913@gmail.com>
2025-02-19 23:27:11 -0500

    Update .tmux.conf Update .config/nvim/lua/plugins/core.lua

--- private_dot_config/nvim/lua/plugins/core.lua
+++ private_dot_config/nvim/lua/plugins/core.lua
@@ -1,21 +1,12 @@
 return {
-
   {
     "EdenEast/nightfox.nvim",
-    opts = {
-      modules = {
-        treesitter = true,
-        whichkey = true,
-        neotree = true,
-        mini = true,
-      },
-    },
   },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin-latte",
-      -- colorscheme = "dayfox",
+      -- colorscheme = "catppuccin-latte",
+      colorscheme = "dayfox",
     },
   },
 }
