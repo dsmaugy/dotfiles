ff8d477d100d34421e84069a2e5e366588bf0bee
Darwin Do <darwin.do@geckorobotics.com>
2025-02-24 19:55:02 -0500

    Update .tmux.conf Update .wezterm.lua

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -5,4 +5,11 @@ config.color_scheme = "dayfox"
 -- config.font = wezterm.font("FiraMono Nerd Font Mono")
 config.font = wezterm.font("JuliaMono")
 -- config.font_size = 13.0
+
+config.hyperlink_rules = wezterm.default_hyperlink_rules()
+
+table.insert(config.hyperlink_rules, {
+	regex = [[(?i)splat-(\d+)]],
+	format = "https://geckorobotics.atlassian.net/browse/SPLAT-$1",
+})
 return config
