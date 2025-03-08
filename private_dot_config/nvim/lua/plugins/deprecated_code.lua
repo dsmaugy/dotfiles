8530704ea29a6449400a860dadfcfeb45e608121
Darwin Do <darwin.do@geckorobotics.com>
2025-03-08 15:35:34 -0500

    Update .config/nvim/lua/plugins/deprecated_code.lua

--- private_dot_config/nvim/lua/plugins/deprecated_code.lua
+++ private_dot_config/nvim/lua/plugins/deprecated_code.lua
@@ -22,32 +22,32 @@ return {
   --     },
   --   },
   -- },
-  -- {
-  --   "nvim-treesitter/nvim-treesitter",
-  --   opts = { ensure_installed = { "glsl", "gotmpl" } },
-  --   ---@param opts TSConfig
-  --   config = function(_, opts)
-  --     if type(opts.ensure_installed) == "table" then
-  --       opts.ensure_installed = LazyVim.dedup(opts.ensure_installed)
-  --     end
-  --     require("nvim-treesitter.configs").setup(opts)
-  --
-  --     vim.filetype.add({
-  --       extension = {
-  --         gotmpl = "gotmpl",
-  --         tmpl = "gotmpl",
-  --       },
-  --     })
-  --
-  --     -- THANK YOU KEVIN NGUYEN
-  --     -- https://github.com/nvim-treesitter/nvim-treesitter/discussions/1917#discussioncomment-10714144
-  --     vim.treesitter.query.add_directive("inject-go-tmpl!", function(_, _, bufnr, _, metadata)
-  --       local fname = vim.fs.basename(vim.api.nvim_buf_get_name(bufnr))
-  --       local _, _, ext, _ = string.find(fname, ".*%.(%a+)(%.%a+)")
-  --       metadata["injection.language"] = ext
-  --     end, {})
-  --   end,
-  -- },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = { ensure_installed = { "glsl", "gotmpl" } },
+    ---@param opts TSConfig
+    config = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        opts.ensure_installed = LazyVim.dedup(opts.ensure_installed)
+      end
+      require("nvim-treesitter.configs").setup(opts)
+
+      vim.filetype.add({
+        extension = {
+          gotmpl = "gotmpl",
+          tmpl = "gotmpl",
+        },
+      })
+
+      -- THANK YOU KEVIN NGUYEN
+      -- https://github.com/nvim-treesitter/nvim-treesitter/discussions/1917#discussioncomment-10714144
+      vim.treesitter.query.add_directive("inject-go-tmpl!", function(_, _, bufnr, _, metadata)
+        local fname = vim.fs.basename(vim.api.nvim_buf_get_name(bufnr))
+        local _, _, ext, _ = string.find(fname, ".*%.(%a+)(%.%a+)")
+        metadata["injection.language"] = ext
+      end, {})
+    end,
+  },
   -- {
   --   "windwp/nvim-ts-autotag",
   --   event = "LazyFile",
