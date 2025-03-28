327df1a517718fbb4983cc17f2140d871f1c0238
Darwin Do <darwin.do@geckorobotics.com>
2025-03-28 14:17:47 -0400

    Update .wezterm.lua

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -1,11 +1,13 @@
 local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
+
 config.color_scheme = "dayfox"
+
 config.font = wezterm.font("JuliaMono")
+config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
 
 config.hyperlink_rules = wezterm.default_hyperlink_rules()
-
 table.insert(config.hyperlink_rules, {
 	regex = [[(?i)splat-(\d+)]],
 	format = "https://geckorobotics.atlassian.net/browse/SPLAT-$1",
@@ -14,6 +16,6 @@ table.insert(config.hyperlink_rules, {
 config.enable_wayland = false
 config.window_background_opacity = 0.93
 config.window_decorations = "TITLE | RESIZE"
-
 config.hide_tab_bar_if_only_one_tab = true
+
 return config
