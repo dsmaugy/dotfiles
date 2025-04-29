a7cd74253c6f0182143930986a4d1c28ca8592db
Darwin Do <darwin78913@gmail.com>
2025-04-28 23:52:40 -0400

    delete

--- private_dot_config/nvim/queries/gotmpl.html/injections.scm
+++
@@ -1,8 +0,0 @@
-;; this doesn't actually work
-;; Inject Go template language inside {{ ... }} blocks
-((template_action) @injection.content
- (#set! injection.language "gotmpl"))
-
-;; Inject Go template language inside {{/* */}} comments
-((comment) @injection.content
- (#set! injection.language "gotmpl"))
