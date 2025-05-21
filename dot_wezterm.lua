c6136259041a42440e3297dd435e5e567d2dcba8
Darwin Do <darwin78913@gmail.com>
2025-05-20 22:55:27 -0400

    Update .wezterm.lua Update .config/hypr/hyprland.conf Update .config/nvim/lazy-lock.json

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -9,11 +9,11 @@ config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
 
 config.hyperlink_rules = wezterm.default_hyperlink_rules()
 table.insert(config.hyperlink_rules, {
-	regex = [[(?i)splat-(\d+)]],
-	format = "https://geckorobotics.atlassian.net/browse/SPLAT-$1",
+  regex = [[(?i)splat-(\d+)]],
+  format = "https://geckorobotics.atlassian.net/browse/SPLAT-$1",
 })
 
-config.enable_wayland = false
+config.enable_wayland = true
 config.window_background_opacity = 0.93
 config.window_decorations = "TITLE | RESIZE"
 config.hide_tab_bar_if_only_one_tab = true
