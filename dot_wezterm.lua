64ad064cf7e30432ba593260f1b31c14cd7a9fd6
Darwin Do <darwin78913@gmail.com>
2025-08-10 17:33:19 -0400

    Update .tmux.conf Update .wezterm.lua Update .config/hypr/hyprland.conf Update .config/nvim/lazy-lock.json Update .config/wezterm/colors/dayfox.toml

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -1,20 +1,25 @@
 local wezterm = require("wezterm")
 
+wezterm.add_to_config_reload_watch_list("/home/darwin/.cache/wal/wezterm-pywal.toml")
+wezterm.add_to_config_reload_watch_list("/home/darwin/.config/wezterm/colors/wezterm-pywal.toml")
+
 local config = wezterm.config_builder()
 
-config.color_scheme = "dayfox"
+-- f
+config.color_scheme = "wezterm-pywal"
+-- config.color_scheme = "dayfox"
 
 config.font = wezterm.font("JuliaMono")
 config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
 
 config.hyperlink_rules = wezterm.default_hyperlink_rules()
 table.insert(config.hyperlink_rules, {
-  regex = [[(?i)splat-(\d+)]],
-  format = "https://geckorobotics.atlassian.net/browse/SPLAT-$1",
+	regex = [[(?i)splat-(\d+)]],
+	format = "https://geckorobotics.atlassian.net/browse/SPLAT-$1",
 })
 
 config.enable_wayland = true
-config.window_background_opacity = 0.93
+config.window_background_opacity = 0.80
 config.window_decorations = "TITLE | RESIZE"
 config.hide_tab_bar_if_only_one_tab = true
 
