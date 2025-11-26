291e8eaa918d7c23482783d4c323096ab9c6319a
Darwin Do <darwin78913@gmail.com>
2025-11-25 20:39:54 -0500

    Update .config/fish/conf.d/common.fish

--- private_dot_config/private_fish/conf.d/common.fish
+++ private_dot_config/private_fish/conf.d/common.fish
@@ -1,9 +1,11 @@
-
 set PYENV_ROOT "$HOME/.pyenv"
 
 fish_add_path /home/darwin/.local/bin
+fish_add_path /home/darwin/go/bin
 
 pyenv init - | source
 
 alias gcm='git checkout main'
 alias ccat='pygmentize -g'
+
+set ELECTRON_OZONE_PLATFORM_HINT wayland
